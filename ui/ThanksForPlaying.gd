fefc3169c4476bef6fb25d9e80c9ca567b5649d5
Tom <tom@smallridge.com.au>
2020-10-05 22:09:34 +0800

    menu and thanksforplaying screens

--- ui/ThanksForPlaying.gd
+++ ui/ThanksForPlaying.gd
@@ -1,7 +1,13 @@
 extends Control
 
+var allow_quit = false
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
-func _on_ExitButton_pressed():
-	get_tree().quit()
+func _process(delta):
+	if allow_quit and Input.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+
+func _on_Timer_timeout():
+	allow_quit = true
